6fc751de5bdab553835ba48ed4c9c758936b742e
Alwin-K-Thomas <alwin_k_thomas@live.in>
2025-09-05 10:39:11 +0100

    Add main kopf startup script

--- src/cr8tor/main.py
+++ src/cr8tor/main.py
@@ -1,34 +1,17 @@
-import debugpy
-from pathlib import Path
-from typing import Annotated
-from dotenv import load_dotenv, find_dotenv
-import typer
-from rocrate.rocrate import ROCrate
+import kopf
+import logging
+from cr8tor.identity import identity_handler  # noqa: F401
+from cr8tor.vdi import vdi_handler  # noqa: F401
 
-# from cr8tor.cli.create import app as cli_commands
-from cr8tor.cli import app as cli
-from cr8tor.cli.display import print_bagit, print_crate
-from cr8tor.cli.initiate import app as cli_initiate
-
-app = typer.Typer()
-app.add_typer(cli)
-app.add_typer(cli_initiate)
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-@app.command(name="read")
-def read_bag(bagit_dir: Annotated[Path, typer.Option(default="-i")] = "./bagit"):
-    """
-    Reads a Research Object Crate (RO-Crate) from the specified directory and prints its details in a table format.
-    """
-
-    print_bagit(bagit_dir)
-    crate = ROCrate(bagit_dir / "data")
-    print_crate(crate)
+@kopf.on.startup()
+def startup_fn(**kwargs):
+    logger.info("Cr8tor Operator is starting up.")
 
 
 if __name__ == "__main__":
-    debugpy.listen(("localhost", 5678))
-    debugpy.wait_for_client()
-
-    load_dotenv(find_dotenv())
-    app()
+    kopf.run()
