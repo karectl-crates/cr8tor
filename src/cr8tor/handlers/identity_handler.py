f5f12642d89f33e65c0280adb55ce6ae25e0af9d
Alwin K Thomas <alwin_k_thomas@live.in>
2025-10-23 18:28:52 +0100

    Fix KeycloakClient protocol mapper handling

--- src/cr8tor/handlers/identity_handler.py
+++ src/cr8tor/handlers/identity_handler.py
@@ -49,7 +49,10 @@ def group_delete(body, spec, meta, **kwargs):
 
 @kopf.on.create("identity.karectl.io", "v1alpha1", "keycloakclient")
 @kopf.on.update("identity.karectl.io", "v1alpha1", "keycloakclient")
+@kopf.on.resume("identity.karectl.io", "v1alpha1", "keycloakclient")
 def client_create_update(body, spec, meta, **kwargs):
+    """Handle KeycloakClient create, update, and resume (on operator restart).
+    """
     client_id = spec["clientId"]
     namespace = meta.get("namespace", "keycloak")
     sync_keycloak_client(client_id, spec, namespace=namespace)
